5f02b3ec2b3aa3db84decdcff3174ce01a1e87b0
oOMCMOo <oomcmoo@gmail.com>
2026-05-11 10:56:27 +0200

    Set base path for GitHub Pages

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/Holographic-DNA-Vault-Upgrade-1.0/',
+  plugins: [react()],
+})
